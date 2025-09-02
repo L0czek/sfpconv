b3539606b31242a40c7e5f7e6558e859ca762a2e
Michał Szaknis <michalszak@gmail.com>
2025-09-02 23:33:45 +0200

    Add build system and boilerplate

---
+++ inc/hal.hpp
@@ -0,0 +1,6 @@
+#ifndef __HAL_HPP__
+#define __HAL_HPP__
+
+#include "stm32g4xx_hal.h"
+
+#endif 
---
+++ inc/sfpconv.hpp
@@ -0,0 +1,8 @@
+#ifndef __SFP_CONV_HPP__
+#define __SFP_CONV_HPP__
+
+class SfpConv {
+
+};
+
+#endif
